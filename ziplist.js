8067115335b17a191e16c320b6e513ff5f033a5d
amaskey <maskey.maskey@gmail.com>
2017-09-18 16:44:26 -1000

    done e26

---
+++ ziplist.js
@@ -0,0 +1,22 @@
+
+
+function ziplist(listOne, listTwo) {
+  const newArray = [];
+  for (let i = 0; i < listOne.length; i++) {
+    newArray.push(listOne[i]);
+    newArray.push(listTwo[i]);
+  }
+  return newArray;
+}
+
+function zipListTheSimpleWay(listOne, listTwo) {
+  const zipped = _.zip(listOne, listTwo);
+  return _.flatten(zipped);
+}
+
+
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+
+console.log(ziplist(list1, list2));
+console.log(zipListTheSimpleWay(list1, list2));
